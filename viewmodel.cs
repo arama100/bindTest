b6b396c49a2880a7145c947715da3c7ed76a5435
arama100 <arama2427@gmail.com>
2018-08-26 23:22:56 +0900

    SDK binding test

--- viewmodel.cs
+++ viewmodel.cs
@@ -9,6 +9,7 @@ namespace test
     {
 
         string str;//変数
+        string str2;
 
         public event PropertyChangedEventHandler PropertyChanged;
         private DispatcherTimer timer;
@@ -24,21 +25,46 @@ namespace test
             }
         }//プロパティ
 
+        public string Str2
+        {
+            get
+            {
+                return this.str;
+            }
+            set
+            {
+                str = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Str2"));
+            }
+        }//プロパティ
+
         public myCommand command { get; private set; }
+        public myCommand command2 { get; private set; }
         public TestViewModel()
         {
             str = "aaa";
-            command = new myCommand(()=>func1());
+            str2 = "bbb";
+            command = new myCommand((bool b)=>func1(b),true);
+            command2 = new myCommand((bool b)=>func2(b),false);
             timer = new DispatcherTimer(DispatcherPriority.Normal);
             timer.Interval = new TimeSpan(0,0,1);
             timer.Tick += new EventHandler(dispatcherTimer_Tick);
             timer.Start();
         }
 
-        void func1()
+        void func1(bool b)
         {
             str = str == "bbb" ? "aaa" : "bbb";
+
             PropertyChanged(this, new PropertyChangedEventArgs("Str"));
+
+        }
+
+        void func2(bool b)
+        {
+            str2 = str2 == "bbb" ? "aaa" : "bbb";
+
+            PropertyChanged(this, new PropertyChangedEventArgs("Str2"));
         }
         void dispatcherTimer_Tick(object sender, EventArgs e)
         {
@@ -50,7 +76,8 @@ namespace test
     public class myCommand : ICommand
     {
         public event EventHandler CanExecuteChanged;
-        private Action act;
+        private Action<bool> act;
+        private bool pram;
         public bool CanExecute(object parameter)
         {
             return true;
@@ -59,13 +86,15 @@ namespace test
 
         public void Execute(object parameter)
         {
-            act();
+
+            act((bool)pram);
             //throw new NotImplementedException();
         }
 
-        public myCommand(Action act)
+        public myCommand(Action<bool> act,bool pram)
         {
             this.act = act;
+            this.pram = pram;
         }
     }
 }
